4cd4ada17d17ff5a41228373dc2634fc63bc73e6
Bruno Hiri <brunobatera7@hotmail.com>
2020-11-27 18:45:43 -0300

    Implementação de gráficos do Dashboard SalesChart.

---
+++ CRMYIA.Business/Dashboard/ValorProducaoPorDiaViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CRMYIA.Business.Util;
+using CRMYIA.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CRMYIA.Business.Dashboard
+{
+    public class ValorProducaoPorDiaViewModel
+    {
+        #region Propriedades
+        public int Dia { get; set; }
+        public string Operadora { get; set; }
+        public decimal Valor { get; set; }
+        #endregion
+
+        #region Construtores
+        #endregion
+
+        #region Métodos
+       
+        #endregion
+    }
+}
