bedfdef8a65dafb49a93f6f349e056a67c7843e1
Allan Cesar <allan.acs.webdeveloper@gmail.com>
2021-06-10 17:22:34 -0300

    KPI 10/06/21 sem carregar indidual no card

---
+++ CRMYIA.Business/KPI/KPIMetaVidaIndividualModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using CRMYIA.Business.Util;
+using CRMYIA.Data.Context;
+using CRMYIA.Data.Entities;
+using CRMYIA.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRMYIA.Business
+{
+    public class KPIMetaVidaIndividualModel
+    {
+        #region Propriedades
+        #endregion
+
+        #region Construtores
+        #endregion
+
+        #region Métodos
+        public static KPIMetaVidaIndividual Get(long IdMetaIndividual)
+        {
+            KPIMetaVidaIndividual Entity = null;
+            try
+            {
+                using (YiaContext context = new YiaContext())
+                {
+                    Entity = context.KPIMetaVidaIndividual
+                        .AsNoTracking()
+                        .Where(x => x.Ativo && x.IdMetaIndividual == IdMetaIndividual)
+                        .AsNoTracking()
+                        .FirstOrDefault();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return Entity;
+        }
+
+        public static List<KPIMetaVidaIndividual> GetList()
+        {
+            List<KPIMetaVidaIndividual> ListEntity = null;
+            try
+            {
+                using (YiaContext context = new YiaContext())
+                {
+                    ListEntity = context.KPIMetaVidaIndividual
+                        .Where(x => x.Ativo)
+                        .AsNoTracking()
+                        .OrderBy(o => o.Descricao).ToList();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return ListEntity;
+        }
+
+        public static void Add(KPIMetaVidaIndividual Entity)
+        {
+            try
+            {
+                using (YiaContext context = new YiaContext())
+                {
+                    context.KPIMetaVidaIndividual.Add(Entity);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public static void Update(KPIMetaVidaIndividual Entity)
+        {
+            try
+            {
+                using (YiaContext context = new YiaContext())
+                {
+                    context.KPIMetaVidaIndividual.Update(Entity);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        #endregion
+    }
+}
--- CRMYIA.Web/wwwroot/js/sistema/novaMeta.js
+++ CRMYIA.Web/wwwroot/js/sistema/novaMeta.js
@@ -1,36 +1,4 @@
-﻿$("#DataMaxima").change(function () {
-    var dataInicial = $("#DataMinima").val();
-    var dataFinal = $("#DataMaxima").val();
-    if (dataInicial > dataFinal) {
-        $("#DataMaxima").val(dataInicial);
-    }
-});
-
-$("#DataMinima").change(function () {
-    var dataInicial = $("#DataMinima").val();
-    var dataFinal = $("#DataMaxima").val();
-    var parts = dataInicial.split("-");
-    if (dataInicial > dataFinal) {
-        $("#DataMaxima").val(dataInicial);
-    }
-    $("#MesValor").val(parts[1]);
-    $("#AnoValor").val(parts[0]);
-    $("#MesVida").val(parts[1]);
-    $("#AnoVida").val(parts[0]);
-});
-
-$("#MesValor").change(function () {
-    var parts = $("#DataMinima").val().split("-");
-    var mes = $("#MesValor").val();
-    if (mes < 1 || mes > 12) {
-        if (parts != "")
-            $("#MesValor").val(parts[1]);
-        else
-            $("#MesValor").val("");
-    }
-});
-
-$("#minValor").change(function () {
+﻿$("#minValor").change(function () {
     var min = $("#minValor").val();
     var max = $("#maxValor").val();
     if (parseFloat(min) >= parseFloat(max)) {
@@ -59,46 +27,5 @@ $("#maxVidas").change(function () {
     var max = $("#maxVidas").val();
     if (parseInt(min) >= parseInt(max)) {
         $("#maxVidas").val("");
-    }
-});
-
-$("#AnoValor").change(function () {
-    var parts = $("#DataMinima").val().split("-");
-    var ano = $("#AnoValor").val();
-    if (ano.lengt != 4) {
-        if (parts != "")
-            $("#AnoValor").val(parts[0]);
-        else
-            $("#AnoValor").val("");
-    }
-});
-
-$("#MesVida").change(function () {
-    var parts = $("#DataMinima").val().split("-");
-    var mes = $("#MesVida").val();
-    if (mes < 1 || mes > 12) {
-        if (parts != "")
-            $("#MesVida").val(parts[1]);
-        else
-            $("#MesVida").val("");
-    }
-});
-
-$("#AnoVida").change(function () {
-    var parts = $("#DataMinima").val().split("-");
-    var ano = $("#AnoVida").val();
-    if (ano.lengt != 4) {
-        if (parts != "")
-            $("#AnoVida").val(parts[0]);
-        else
-            $("#AnoVida").val("");
-    }
-});
-
-$("#DataMinima").change(function () {
-    var dataInicial = $("#DataMinima").val();
-    var dataFinal = $("#DataMaxima").val();
-    if (dataInicial > dataFinal) {
-        $("#DataMaxima").val(dataInicial);
     }
 });
